d86e08eda81bc60dbcd903fb09963238f2ae248d
Bruno P. Kinoshita <brunodepaulak@yahoo.com.br>
2015-02-03 22:15:12 -0200

    Myers package

---
+++ src/main/java/br/eti/kinoshita/myers/package-info.java
@@ -0,0 +1,5 @@
+/**
+ * Playing with the Myers Algorithm migrated from Commons Collections to
+ * Commons Text - https://issues.apache.org/jira/browse/SANDBOX-486
+ */
+package br.eti.kinoshita.myers;
